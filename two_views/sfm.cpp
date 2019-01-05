f79d4c9ceb9ffa78261e01a8553e9f98352cc1d0
Yun <yunchang@mit.edu>
2019-01-05 04:54:22 -0500

    add new proj

---
+++ two_views/sfm.cpp
@@ -0,0 +1,8 @@
+/*
+Corresponding to chapter 7 of book 
+Given a collection of image paris correspoinding to 
+an unknown number of indepedent and rigidly moving 
+objects, estimate the number of independent motions, 
+the fundamental matrices, and the object to which 
+each image pair belongs
+*/ 
